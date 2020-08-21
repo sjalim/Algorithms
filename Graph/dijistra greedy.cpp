f132fe9e8680ed5233510f723b6b1448081014d3
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2020-08-21 16:14:23 +0600

    dijistra path find update

--- Graph/dijistra greedy.cpp
+++ Graph/dijistra greedy.cpp
@@ -5,7 +5,7 @@ int ans[100];
 int miniDistUnvisited(int dist[], bool visit[], int v)
 {
     int mini = INT_MAX, idx;
-    for (int i = 0; i < v; i++)
+    for (int i = 1; i <= v; i++)
     {
         if (!visit[i] && mini > dist[i])
         {
@@ -15,12 +15,38 @@ int miniDistUnvisited(int dist[], bool visit[], int v)
     }
     return idx;
 }
+
+int printSolution(int dis[], int n, int parent[], int src)
+{
+
+    cout << "NODE:\t\t\tCOST:\t\t\tPATH\n";
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << "\t\t\t" << dis[i] << "\t\t\t"
+             << " ";
+        cout << i << " ";
+        int p = parent[i];
+        while (p)
+        {
+            
+         
+            cout << "<--" << p << " ";
+               p = parent[p];
+        }
+        cout << endl;
+    }
+}
+
 void dijistra(int s, int v)
 {
-    int dist[v];
-    bool visit[v];
-    for (int i = 0; i < v; i++)
+    int dist[v + 1];
+    bool visit[v + 1];
+    int parent[v + 1];
+    parent[s] = 0;
+ 
+    for (int i = 1; i <= v; i++)
     {
+
         dist[i] = INT_MAX;
         visit[i] = false;
     }
@@ -29,26 +55,29 @@ void dijistra(int s, int v)
     {
         int u = miniDistUnvisited(dist, visit, v);
         visit[u] = true;
-        for (int j = 0; j < v; j++)
+        for (int j = 1; j <= v; j++)
         {
             if (!visit[j] && G[u][j] && dist[u] != INT_MAX && dist[j] > dist[u] + G[u][j])
             {
+                parent[j] = u;
                 dist[j] = dist[u] + G[u][j];
             }
         }
     }
-    for (int i = 0; i < v; i++)
-    {
-        cout << s << "-->>" << i << "="<< dist[i] << endl;
-    }
+
+    printSolution(dist, v, parent, s);
+    // for (int i = 0; i < v; i++)
+    // {
+    //     cout << s << "-->>" << i << "=" << dist[i] << endl;
+    // }
 }
 int main()
 {
 
-    int e, v=9;
+    int e, v = 9;
     int a, b, w;
     cin >> v >> e;
-    for(int i=0;i<e;i++)
+    for (int i = 0; i < e; i++)
     {
         cin >> a >> b >> w;
         G[a][b] = w;
@@ -59,3 +88,11 @@ int main()
     cin >> s;
     dijistra(s, v);
 }
+/*5 6
+1 5 100
+1 3 30
+1 2 10
+3 2 50
+2 4 20
+5 4 60
+1 */
