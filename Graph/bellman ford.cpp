0f3a015de7bb72216d46f1d098ab77ccfb0ebd3d
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2020-08-21 20:56:15 +0600

    bellman ford path find update

--- Graph/bellman ford.cpp
+++ Graph/bellman ford.cpp
@@ -3,7 +3,27 @@ using namespace std;
 
 vector<int> v[2000 + 10];
 int dis[1000 + 10];
-void bellmanford(int n, int m,int s)
+
+int parent[100];
+void pathFinder(int n,int s)
+{
+    cout << "Node\t\t\t\tDistance\t\t\t\tpath" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << s<<"-->"<<i << "\t\t\t\t" << dis[i]<<"\t\t\t\t"<<i;
+
+        int p = parent[i];
+
+        while (p != -1)
+        {
+
+            cout << "<--" << p << " ";
+            p = parent[p];
+        }
+        cout << endl;
+    }
+}
+void bellmanford(int n, int m, int s)
 {
 
     for (int i = 0; i < m + 2; i++)
@@ -12,18 +32,18 @@ void bellmanford(int n, int m,int s)
         v[i].clear();
         dis[i] = 2e9;
     }
-    int from, next, weight;
+    int a, b, w;
     for (int i = 0; i < m; i++)
     {
+        cin >> a >> b >> w;
 
-        scanf("%d%d%d", &from, &next, &weight);
-
-        v[i].push_back(from-1);
-        v[i].push_back(next-1);
-        v[i].push_back(weight);
+        v[i].push_back(a);
+        v[i].push_back(b);
+        v[i].push_back(w);
     }
 
-    dis[0] = 0;
+    dis[s] = 0;
+    parent[s] = -1;
     for (int i = 0; i < n - 1; i++)
     {
         int j = 0;
@@ -32,21 +52,39 @@ void bellmanford(int n, int m,int s)
 
             if (dis[v[j][0]] + v[j][2] < dis[v[j][1]])
             {
+                parent[v[j][1]] = v[j][0];
                 dis[v[j][1]] = dis[v[j][0]] + v[j][2];
             }
             j++;
         }
     }
+    pathFinder(n,s);
 }
 int main()
 {
 
-    int m, n,s;
+    int m, n, s;
+    cout << "Enter Number of vertex and edges:";
     cin >> n >> m;
+    cout << "Enter source:";
     cin >> s;
-    bellmanford(n, m,s);
-    for (int i = 0; i < n; i++)
-    {
-        cout << i+1 << "-->" << dis[i] << endl;
-    }
+    bellmanford(n, m, s);
+
 }
+
+
+// 0 indexed vertexs
+/* 
+7 10
+0
+0 1 6
+0 2 5
+0 3 5
+3 2 -2
+2 1 -2
+1 4 -1
+2 4 1
+3 5 -1
+5 6 3
+4 6 3
+ */
